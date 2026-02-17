14473af50ee525698a81e30174bee4c910814f74
Hiya Jayswal <87514873+hiya1801@users.noreply.github.com>
2026-02-16 19:40:46 -0500

    Add DOM element selections for RGB sliders and HTML element
    Added event listener for DOMContentLoaded and selected RGB sliders.

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -1,3 +1,12 @@
 /** Lab 2 - Page Color Changer (RGB Sliders)
  *  Student: Hiyaben Haresbhai Jayswal
  *  Student ID: 200578344 */
+document.addEventListener("DOMContentLoaded", () => {
+  // 1) DOM ELEMENT SELECTION
+  // Range sliders (input elements)
+  const redSlider = document.querySelector("#red");
+  const greenSlider = document.querySelector("#green");
+  const blueSlider = document.querySelector("#blue");
+
+  // document.documentElement is the <html> element.
+  const htmlElement = document.documentElement;
