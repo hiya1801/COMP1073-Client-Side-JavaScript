16780e3b65d72a2e71bcf3a1b0c4b42df1c982e6
Hiya Jayswal <87514873+hiya1801@users.noreply.github.com>
2026-03-22 13:24:07 -0400

    Implement showTopFlavors function
    Added showTopFlavors function to display flavors.

--- Lab_4/js/json.js
+++ Lab_4/js/json.js
@@ -29,3 +29,46 @@ function populateHeader(data) {
     header.appendChild(h1);
     header.appendChild(p);
 }
+
+// Display all flavors
+function showTopFlavors(data) {
+    const flavors = data.topFlavors;
+
+    for (let i = 0; i < flavors.length; i++) {
+        const article = document.createElement('article');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = flavors[i].name;
+
+        const img = document.createElement('img');
+        img.src = './images/' + flavors[i].image;
+
+        // Calories badge
+        const calories = document.createElement('span');
+        calories.className = 'badge calories';
+        calories.textContent = `Calories: ${flavors[i].calories}`;
+
+        // Type badge
+        const type = document.createElement('span');
+        type.className = 'badge type';
+        type.textContent = flavors[i].type;
+
+        // Ingredients list
+        const ul = document.createElement('ul');
+        flavors[i].ingredients.forEach(ingredient => {
+            const li = document.createElement('li');
+            li.textContent = ingredient;
+            ul.appendChild(li);
+        });
+
+        // Append elements to card
+        article.appendChild(h2);
+        article.appendChild(img);
+        article.appendChild(calories);
+        article.appendChild(type);
+        article.appendChild(ul);
+
+        // Append card to section
+        section.appendChild(article);
+    }
+}
